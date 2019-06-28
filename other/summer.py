206dfd8347467195ef1dd4145b1c1beba1c737ba
Jefferson464 <51965141+Jefferson464@users.noreply.github.com>
2019-06-28 12:10:06 -0700

    Update summer.py

--- other/summer.py
+++ other/summer.py
@@ -1,2 +1,3 @@
-<!--Clue #2: What language is spoke in the nation where the mountain is located?-->
+Clue #2: What language is spoke in the nation where the mountain is located?
 バナナ
+Clue #3: Go back to where you started. Then, get to the image.
