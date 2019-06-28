82601fbd20ec7295ad6821819d8c0bef8818c271
Jefferson464 <51965141+Jefferson464@users.noreply.github.com>
2019-06-28 12:38:28 -0700

    Update summer.py

--- other/summer.py
+++ other/summer.py
@@ -1,3 +1,4 @@
-Clue #2: What language is spoke in the nation where the mountain is located?
+Clue #2: What is the national language in the nation where the mountain is located?
 バナナ
 Clue #3: Go back to where you started. Then, get to the image.
+By the way, if you are here before you got the right mountain, then go back. You need that for clue #2.
